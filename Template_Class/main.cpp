c2da0f45c5fa2c247d3a9bbcae58a85c233c8bc1
Alex Untilov <untiloval80@gmail.com>
2023-03-02 18:15:09 +0100

    Initial Commit

---
+++ Template_Class/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+    int x;
+    int y;
+public:
+    Point(){
+        x = 0;
+        y = 0;
+    }
+    Point operator+=(Point& p){
+        this->x += p.x;
+        this->y += p.y;
+        return *this;
+    }
+};
+
+template<class T, int size>
+class MyArray{
+    T* arr = new T[size];
+public:
+    MyArray(){
+        arr = nullptr;
+    }
+    ~MyArray(){
+        delete[] arr;
+    }
+    void sum(){
+        T sum;
+        for (int i; i < size; i++){
+            sum += arr[i];
+        }
+    }
+};
+
+int main() {
+    
+}
